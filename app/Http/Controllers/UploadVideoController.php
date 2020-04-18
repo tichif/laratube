290f72deb65948a93d89bb7c6fb02cc53a3214a5
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-04-18 15:52:39 -0400

    -Create Jobs for video converter - Install ffmpeg and configure it on the computer - Install laravel-ffmpeg plugin - Create job and queue -Change the queue_connection in .env to database and use php artisan config:cache to clear the cache

--- app/Http/Controllers/UploadVideoController.php
+++ app/Http/Controllers/UploadVideoController.php
@@ -3,6 +3,7 @@
 namespace Laratube\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laratube\Jobs\Videos\ConvertForStreaming;
 use Laratube\Channel;
 
 class UploadVideoController extends Controller
@@ -12,10 +13,14 @@ class UploadVideoController extends Controller
     }
 
     public function store(Request $request, Channel $channel){
-        return $channel->videos()->create([
+        $video = $channel->videos()->create([
             'channel_id' => $channel->id,
             'title' => $request->title,
             'path' => $request->video->store("channel/{$channel->id}/videos")
         ]);
+
+        $this->dispatch(new ConvertForStreaming($video));
+
+        return $video ;
     }
 }
